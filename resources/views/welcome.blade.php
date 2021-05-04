c39dc2b362ef20c73cf8248e86f4799114bfd38b
jesus <jesus_lopez1997@hotmail.com>
2021-05-04 18:29:26 -0400

    setting up templates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')
 
-    <title>Laravel</title>
+@section('content')
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
-    <!-- Styles -->
+<div>Hi there</div>
 
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-
-    </style>
-</head>
-<body>
-    <div class="main">
-
-    </div>
-</body>
-</html>
+@endsection
