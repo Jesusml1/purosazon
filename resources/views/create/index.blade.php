2ae521dedd5440313bf41c4bd43495f6b5bc553e
jesus <jesus_lopez1997@hotmail.com>
2021-05-06 18:12:30 -0400

    adding submition norms to create page

--- resources/views/create/index.blade.php
+++ resources/views/create/index.blade.php
@@ -4,6 +4,64 @@
 @section('content')
 
 <div class="content-form">
+
+
+    <div class='rules-container'>
+        <details>
+            <summary>Normas para la sumision de recetas</summary>
+            <section>
+                <p>
+                    El NOMBRE de la receta titene que ser descriptivo.
+                    Ej: “Bistec a la plancha con vegetales”.
+                </p>
+                <br>
+                <p>
+                    Si su receta no entra dentro de ninguna de las categorias
+                    especificadas, seleccione la opcion “otro”.
+                </p>
+                <br>
+                <p>
+                    La DESCRIPCION es opcional, pero sirve para proporcionar
+                    informacion acerca de la duracion, dificultad u otros
+                    detalles referente a la receta (no incluir historias).
+                </p>
+                <br>
+                <p>
+                    Colocar los INGREDIENTES separados por lineas con cantidades
+                    en expresadas en el sistema de unidades (metrico y/o imperial),
+                    ej:
+                    <br>
+                    <br>
+                    1/4 taza de harina
+                    <br>
+                    2 huevos
+                    <br>
+                    1/3 taza azucar
+                    <br>
+                    <br>
+                    Se desaconseja el uso de unidades de medida ambiguas
+                    (“un poco”, “una pizca”, etc).
+                </p>
+                <br>
+                <p>
+                    En PREPARACION se desean instrucciones secuenciales y
+                    claras en cuanto a la elaboracion de la receta. Se
+                    recomienda ser lo mas conciso posible.
+                </p>
+                <br>
+                <p>
+                    <strong>
+                        El sitio se reserva el derecho a remover cualquer receta que
+                        no cumpla con las normas y/o ya este publicada.
+                    </strong>
+                </p>
+                <br>
+
+            </section>
+        </details>
+    </div>
+
+
     <div class="form-container">
         <h2>Agrega una Receta</h2>
         <form action="/recepie" method="POST">
