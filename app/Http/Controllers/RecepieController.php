3fb3432f34734517ba1e89dd7069d966c7ebad31
jesus <jesus_lopez1997@hotmail.com>
2021-05-08 19:45:02 -0400

    removing unused variable

--- app/Http/Controllers/RecepieController.php
+++ app/Http/Controllers/RecepieController.php
@@ -29,7 +29,7 @@ class RecepieController extends Controller
         ]);
     }
 
-    public function search(Request $request)
+    public function search()
     {
         $query = request('query');
         $category = request('category');
